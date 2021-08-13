1efd07a03f539d22c62757cd0cd5d2fe66e68898
GillesRitchmond <gritchmond@gmail.com>
2021-08-13 14:41:37 -0400

    More update

--- Model/connection.php
+++ Model/connection.php
@@ -1,21 +1,21 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "quick-computer-db";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "quick-computer-db";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// $conn = new mysqli($servername, $username, $password, $dbname);
 
-// $servername = 'mysql-42515-0.cloudclusters.net';
-// $dbname   = 'quick_computer_db';
-// $username = 'admin';
-// $password = 'Hl62SW6d';
-// $dbServerPort = "19457";
-// $charset = 'utf8mb4';
+$servername = 'mysql-42515-0.cloudclusters.net';
+$dbname   = 'quick_computer_db';
+$username = 'admin';
+$password = 'Hl62SW6d';
+$dbServerPort = "19457";
+$charset = 'utf8mb4';
 
 // Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname, $dbServerPort);
+$conn = new mysqli($servername, $username, $password, $dbname, $dbServerPort);
 
 // Check connection
 if (!$conn) {
